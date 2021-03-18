d8a1d726fc62273662f9ce0bbe34059e7d5e65e5
Luisa <massimo.e.luisa.au@gmail.com>
2021-03-18 10:04:09 +0100

    list

---
+++ src/LinkedListed/LinkedList.java
@@ -0,0 +1,4 @@
+package LinkedListed;
+
+public class LinkedList {
+}
---
+++ src/LinkedListed/Node.java
@@ -0,0 +1,7 @@
+package LinkedListed;
+
+public class Node {
+    // Er kann Ware aufnehmen
+    int value;// Ware
+    Node next;  //Wir kennen unsere Nachfolger
+}
